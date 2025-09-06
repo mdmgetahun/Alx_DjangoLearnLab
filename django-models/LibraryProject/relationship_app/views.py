2491d64c883cae83deee21d02a146bc1264f1fd4
mdgetahun <henokmgetahun@gmail.com>
2025-09-06 09:32:17 +0200

    Editeed views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
 from .models import Book
 from .models import Library
+from django.views.generic.detail import DetailView
 
 
 def list_books(request):
