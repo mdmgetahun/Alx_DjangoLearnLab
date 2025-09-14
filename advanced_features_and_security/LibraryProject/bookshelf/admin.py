251cd58259dfdad1325e6433cfaeccbcb73e783e
mdgetahun <henokmgetahun@gmail.com>
2025-09-14 16:07:59 +0200

    Duplicated django medels and added login options

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Author
 from .models import Book
 
@@ -15,4 +16,15 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("author", "publication_year")
     search_fields = ("title", "author")
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 # Register your models here.
