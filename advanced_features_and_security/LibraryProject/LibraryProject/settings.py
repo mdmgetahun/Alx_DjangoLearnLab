b1794544fa089f5d4b6aefbab65daebd6508c460
mdgetahun <henokmgetahun@gmail.com>
2025-09-14 16:10:27 +0200

    Duplicated django medels and added login options

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -101,7 +101,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = [
+                    'accounts.CustomUser'
+                    'bookshelf.CustomUser'
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
