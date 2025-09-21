2c2a814ea9d5fe32cb3006d906fd31d2dcea6624
mdgetahun <henokmgetahun@gmail.com>
2025-09-21 20:00:24 +0200

    fixed a spelling error

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthetication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
