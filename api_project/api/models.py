b9a2d3934c269abc387da401fd098f6d02db7a19
mdgetahun <henokmgetahun@gmail.com>
2025-09-21 18:54:07 +0200

    Create the Serializer, Api view and tested the api for the module Book

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
-class BooK(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    
+    def __str__(self):
+        return self.name
+ 
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+ 
 # Create your models here.
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
 
+class BookList(generics.ListAPIView):
+    querysets = Book.objects.all()
+    serializer = BookSerializer
 # Create your views here.
