c5b70594c9a3d2e3bf9184575a91b9ed2a83adbd
mdgetahun <henokmgetahun@gmail.com>
2025-09-21 19:24:34 +0200

    added a missing bracket in urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,7 +3,7 @@ from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
-router= DefaultRouter
+router= DefaultRouter()
 router.register(r'books_all', BookViewSet, base_name='book_all')
 
 
