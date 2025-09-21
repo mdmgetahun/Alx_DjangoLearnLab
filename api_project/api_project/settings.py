a0659bf198d5cc9020b4fdc7cf2f226281aa923c
mdgetahun <henokmgetahun@gmail.com>
2025-09-21 20:02:07 +0200

    fixed a spelling error

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -108,7 +108,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest.framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
+        ''
     ]
 }
 
