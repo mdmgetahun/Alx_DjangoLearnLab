4dfae74e77836993b62ba179f7eb93e8e2ab9a17
mdgetahun <henokmgetahun@gmail.com>
2025-09-21 22:57:25 +0200

    added form example in the bookshelf folder

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -11,3 +11,4 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'date_of_birth', 'profile_photo')
+        ["ExampleForm"]
