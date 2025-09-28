21315fb88b85ae0f3ef8f9aa0b4ca8797e636cb6
mdmgetahun <you@example.com>
2025-09-28 21:31:12 +0200

    Added filtering based on author to the list view

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book_list') #List all books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail') #path to get details of a specific book
     path('books/create/', BookCreateView.as_view(), name='book_create') #path to create a new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book_update') #path to update a specific book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete')#path to delete a specific book
+    path('books/update/', BookUpdateView.as_view(), name='book_update') #path to update a specific book
+    path('books/delete/', BookDeleteView.as_view(), name='book_delete')#path to delete a specific book
 
 ]
