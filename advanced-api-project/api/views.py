2df760ace75476ae6c57bde57ac946f450037ff0
mdmgetahun <you@example.com>
2025-09-28 21:29:41 +0200

    Added filtering based on author to the list view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import permissions, filters
 
 class BookListView(generics.ListAPIView): 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny] #Anyone can view the list of books
+
+   def get_queryset(self):
+        queryset = Book.objects.all()
+        #Filter by author
+        author_id = self.request.query_params.get('author')
+        if author_id:
+            queryset = queryset.filter(author__id=author_id)
+
+        #Filter by published year
+        published_year = self.request.query_params.get('published_year')
+        if published_year:
+            queryset = queryset.filter(published_year=published_year)
+        return queryset
 
 
 class BookDetailView(generics.RetrieveAPIView):
