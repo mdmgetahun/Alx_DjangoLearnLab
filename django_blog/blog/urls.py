57f34fc465f45ed86af8b0b6c189a4857d794eb1
mdgetahun <henokmgetahun@gmail.com>
2025-10-02 23:26:38 +0200

    extend django custom user creation, added vies for the login, logout, register and profile page in addition to creating templates for them

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .views import UserLoginView, RegisterView, UserLogoutView, home_view, posts_view,  profile_view
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('posts/', posts_view, name='posts'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', profile_view, name='profile'),
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponse
+from django.views.generic import TemplateView, FormView
+from .forms import CustomUserCreationForm
+from django.contrib import messages
+from django.urls import reverse_lazy
 
+class UserLoginView(LoginView): #custom login view
+    template_name = 'blog/login.html'
+    success_url = reverse_lazy('profile') # redirect to profile page after successful login
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('home') # redirect to home page after logout
+
+class RegisterView(FormView): #custom register view
+    template_name = 'blog/register.html'
+    form_class = CustomUserCreationForm 
+    success_url = reverse_lazy('login') # redirect to login page after successful registration
+
+    def form_valid(self, form): 
+        form.save()
+        messages.success(self.request, "Success")
+        return super().form_valid(form)
+
+def home_view(request):
+    return HttpResponse("home")
+
+def posts_view(request):
+    return HttpResponse("Post here!")
+
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Successfully created account!")
+#             return redirect('login')
+#     else:
+#         form = CustomUserCreationForm()  
+
+#     return render(request, 'blog/register.html', {'form': form})
+
+def profile_view(request):
+    return render(request, 'blog/profile.html')
 # Create your views here.
