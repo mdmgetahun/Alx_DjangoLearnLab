a9d8cf4aba3d56709fbf25798c1a8ee9abb68090
mdgetahun <henokmgetahun@gmail.com>
2025-10-02 17:17:55 +0200

    Changed my database to mysql and edited my setting.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'USER': 'django_user',
+        'PASSWORD': 'superuser',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
