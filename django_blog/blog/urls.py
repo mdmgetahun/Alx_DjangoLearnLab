88b66d21307146e5fda2c7aaf9fd7c72b2ec9200
mdgetahun <henokmgetahun@gmail.com>
2025-10-05 22:30:06 +0200

    corrected a url for the Comment Create View

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,10 +13,11 @@ urlpatterns = [
     path('post/', PostListView.as_view(), name='posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('comment/<int:pk>/new/', CommentCreateView.as_view(), name='comment_update'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
